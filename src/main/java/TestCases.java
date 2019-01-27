11f0a6bc4fe655cb9023addbd358af0c11a67f33
Mike <Mike@mywork.com>
2019-01-28 00:34:44 +0530

    inital commit with all project changes

---
+++ src/main/java/TestCases.java
@@ -0,0 +1,19 @@
+package main.java;
+
+import org.testng.annotations.*;
+public class TestCases {
+	
+@Test
+public void test1()
+{
+	System.out.println("Test 1 is passed ");
+}
+
+@Test
+public void test2()
+{
+	System.out.println("Test 2 is passed ");
+}
+
+	
+}
